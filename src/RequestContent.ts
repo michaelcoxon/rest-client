1a4e12f8d0859bcb6d9ce3a28449b7bcffbaf5c9
Michael Coxon <michaelcoxon@outlook.com.au>
2018-03-26 19:17:59 +0800

    added a few more tests and a string content handler

--- src/RequestContent.ts
+++ src/RequestContent.ts
@@ -51,3 +51,26 @@ export class JsonRequestContent<T> implements IHttpRequestContent
         return await contentWriter(json);
     }
 }
+
+export class StringRequestContent implements IHttpRequestContent
+{
+    readonly headers: IHttpContentHeaderCollection;
+    readonly str: string;
+
+    constructor(str: string, headers: IHttpContentHeaderCollection = new HttpContentHeaderCollection())
+    {
+        this.str = str;
+
+        headers.contentType = {
+            contentType: KnownContentTypes.plainText,
+            encoding: HttpContentEncoding.utf8,
+        };
+
+        this.headers = headers;
+    }
+
+    public async executeAsync<TResult>(contentWriter: (data?: any) => TResult | PromiseLike<TResult>): Promise<TResult>
+    {
+        return await contentWriter(this.str);
+    }
+}
--- tests/Xhr.spec.ts
+++ tests/Xhr.spec.ts
@@ -8,6 +8,7 @@ import { XhrHttpClient } from '../src/xhr/XhrHttpClient';
 import { BasicAuthenticationFilter } from '../src/filters/BasicAuthenticationFilter';
 import { HttpMethod, HttpStatusCode } from '../src/interfaces/HttpClientEnums';
 import { JsonResponseContent } from '../src/response-content/JsonResponseContent';
+import { JsonRequestContent, StringRequestContent } from '../src/RequestContent';
 
 //(<any>global).XMLHttpRequest = xhrm.XMLHttpRequest
 
@@ -24,7 +25,7 @@ describe('XhrHttpClient', () =>
         const httpClient = new XhrHttpClient();
 
         const response = await httpClient
-            .createRequest(HttpMethod.get, 'http://httpbin.org/get')
+            .createRequest(HttpMethod.get, 'https://httpbin.org/get')
             .executeAsync();
 
         if (!response)
@@ -41,7 +42,7 @@ describe('XhrHttpClient', () =>
 
         const obj = content.toObject<{ url: string }>();
 
-        assert(obj.url === "http://httpbin.org/get");
+        assert(obj.url === "https://httpbin.org/get");
     });
 
     it('does basic http get request with basic auth', async () =>
@@ -53,7 +54,7 @@ describe('XhrHttpClient', () =>
         const httpClient = new XhrHttpClient([authHandler]);
 
         const response = await httpClient
-            .createRequest(HttpMethod.get, 'http://httpbin.org/get')
+            .createRequest(HttpMethod.get, 'https://httpbin.org/get')
             .executeAsync();
 
         if (!response)
@@ -72,6 +73,62 @@ describe('XhrHttpClient', () =>
         const creds = atob(obj.headers.Authorization.substring('Basic '.length));
 
         assert(creds === `${username}:${password}`);
-        assert(obj.url === "http://httpbin.org/get");
+        assert(obj.url === "https://httpbin.org/get");
+    });
+
+    it('does basic get request with redirects', async () =>
+    {
+        const url = "https://httpbin.org/redirect-to?url=" + encodeURIComponent("https://httpbin.org/get");
+        const httpClient = new XhrHttpClient();
+
+        const response = await httpClient
+            .createRequest(HttpMethod.get, url)
+            .executeAsync();
+
+        if (!response)
+        {
+            throw new Error('request was cancelled');
+        }
+
+        assert(response.status == HttpStatusCode.ok, "status code should be 200");
+        assert(response.ok, "ok should be true");
+
+        const content = (await response.contentAsync) as JsonResponseContent;
+
+        assert(content instanceof JsonResponseContent, "content should be json");
+
+        const obj = content.toObject<{ url: string }>();
+
+        assert(obj.url === "https://httpbin.org/get", "url was not correct");
+    });
+
+    it('does basic http post request', async () =>
+    {
+        const strContent: string = 'Hello World!';
+        const url = 'http://httpbin.org/post';
+        const httpClient = new XhrHttpClient();
+
+        const request = httpClient.createRequest(HttpMethod.post, url);
+        request.content = new StringRequestContent(strContent);
+
+        const response = await request.executeAsync();
+
+        if (!response)
+        {
+            throw new Error('request was cancelled');
+        }
+
+        assert(response.status == HttpStatusCode.ok, "status code should be 200");
+        assert(response.ok, "ok should be true");
+
+        const content = (await response.contentAsync) as JsonResponseContent;
+
+        assert(content instanceof JsonResponseContent, "content should be json");
+
+        const obj = content.toObject<{ url: string, data: string }>();
+
+        assert(obj.data, "data is undefined");
+        assert(obj.data === strContent, `content not same: '${JSON.stringify(obj.data)}'`);
+        assert(obj.url === "http://httpbin.org/post", "url was not correct");
     });
 });
