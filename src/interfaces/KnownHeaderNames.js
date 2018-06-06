63f14678e7c761762c74236273e370e87be93efe
Michael Coxon <michaelcoxon@outlook.com.au>
2018-06-07 00:35:29 +0800

    delete js files

--- src/interfaces/KnownHeaderNames.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var KnownHeaderNames;
-(function (KnownHeaderNames) {
-    KnownHeaderNames["accept"] = "Accept";
-    KnownHeaderNames["authorization"] = "Authorization";
-    KnownHeaderNames["cacheControl"] = "Cache-Control";
-    KnownHeaderNames["contentType"] = "Content-Type";
-})(KnownHeaderNames = exports.KnownHeaderNames || (exports.KnownHeaderNames = {}));
