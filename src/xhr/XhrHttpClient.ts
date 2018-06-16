6f412396ce6b1eff32a18ce892b185ddc7383ced
Michael Coxon <michaelcoxon@outlook.com.au>
2018-06-16 19:10:05 +0800

    add documentation and optimise outputs

--- src/xhr/XhrHttpClient.ts
+++ src/xhr/XhrHttpClient.ts
@@ -4,30 +4,64 @@ import { XhrHttpRequest, XhrHttpResponse } from "./XhrHttpRequestResponse";
 import { JsonResponseContent } from "../ResponseContent";
 import { StringOrUrl, Url } from "../Url";
 import { HttpRequestHeaderCollection } from "../HttpRequestHeaderCollection";
-import { RequestCancelledException, ServiceException } from "..";
+import { RequestCancelledException, ServiceException, JsonRequestContent } from "..";
 
 
-
+/** Implementation of IHttpClient for XMLHttpRequest */
 export class XhrHttpClient implements IHttpClient
 {
+    /** The default timeout for requests. default is 5000ms */
     public static DefaultTimeout: number = 5000;
+    /** The default ignoreCache parameter for XHR. default is false */
     public static DefaultIgnoreCache: boolean = false;
+    /** The default headers to use. defaults to Accept: application/json, text/javascript, text/plain */
     public static DefaultHeaders: IHttpRequestHeaderCollection = new HttpRequestHeaderCollection(...[{
         name: KnownHeaderNames.accept,
         value: "application/json, text/javascript, text/plain"
     }]);
 
+    /** Collections of filters to use for the request/response */
     public readonly filters: IHttpFilter[];
 
     private readonly _headers: IHttpRequestHeaderCollection;
     private readonly _ignoreCache: boolean;
     private readonly _timeout: number;
 
+    /** default constructor */
     constructor();
+    /**
+     * constructor
+     * @param filters filters for the request/response
+     */
     constructor(filters: IHttpFilter[]);
+    /**
+     * constructor
+     * @param filters filters for the request/response
+     * @param headers headers for the request
+     */
     constructor(filters: IHttpFilter[], headers: IHttpRequestHeaderCollection);
+    /**
+     * constructor
+     * @param filters filters for the request/response
+     * @param headers headers for the request
+     * @param ignoreCache the ignore cache setting. true means we will always get a new value, false means we will respect the servers cache settings.
+     */
     constructor(filters: IHttpFilter[], headers: IHttpRequestHeaderCollection, ignoreCache: boolean);
+    /**
+     * constructor
+     * @param filters filters for the request/response
+     * @param headers headers for the request
+     * @param ignoreCache the ignore cache setting. true means we will always get a new value, false means we will respect the servers cache settings.
+     * @param timeout the timeout for the request
+     */
     constructor(filters: IHttpFilter[], headers: IHttpRequestHeaderCollection, ignoreCache: boolean, timeout: number);
+    /**
+     * constructor
+     * @param filters filters for the request/response
+     * @param headers headers for the request
+     * @param ignoreCache the ignore cache setting. true means we will always get a new value, false means we will respect the servers cache settings.
+     * @param timeout the timeout for the request
+     */
     constructor(filters?: IHttpFilter[], headers?: IHttpRequestHeaderCollection, ignoreCache?: boolean, timeout?: number);
     constructor(filters?: IHttpFilter[], headers?: IHttpRequestHeaderCollection, ignoreCache?: boolean, timeout?: number)
     {
@@ -37,8 +71,23 @@ export class XhrHttpClient implements IHttpClient
         this._timeout = timeout === undefined ? XhrHttpClient.DefaultTimeout : timeout;
     }
 
+    /**
+     * Creates a new request
+     * @param method the HTTP method to use
+     * @param uri the resource uri
+     */
     createRequest(method: HttpMethod, uri: string): IHttpRequest;
+    /**
+     * Creates a new request
+     * @param method the HTTP method to use
+     * @param uri the resource uri
+     */
     createRequest(method: HttpMethod, uri: Url): IHttpRequest;
+    /**
+     * Creates a new request
+     * @param method the HTTP method to use
+     * @param uri the resource uri
+     */
     createRequest(method: HttpMethod, uri: StringOrUrl): IHttpRequest;
     createRequest(method: HttpMethod, uri: StringOrUrl): IHttpRequest
     {
@@ -88,4 +137,47 @@ export class XhrHttpClient implements IHttpClient
         return content.toObject<T>();
     }
 
+    /**
+     * Performs a post request to a JSON returning source and casts the object
+     * @param uri as string
+     * @throws RequestCancelledException when the request is cancelled
+     * @throws ServiceException if the response is cancelled or if there was a status code in the error range
+     */
+    public async postObjectAsync<TData, TResponseData>(uri: string, data: TData): Promise<TResponseData>;
+    /**
+     * Performs a post request to a JSON returning source and casts the object
+     * @param uri as Uri
+     * @throws RequestCancelledException when the request is cancelled
+     * @throws ServiceException if the response is cancelled or if there was a status code in the error range
+     */
+    public async postObjectAsync<TData, TResponseData>(uri: Url, data: TData): Promise<TResponseData>;
+    /**
+     * Performs a post request to a JSON returning source and casts the object
+     * @param uri as string or Uri
+     * @throws RequestCancelledException when the request is cancelled
+     * @throws ServiceException if the response is cancelled or if there was a status code in the error range
+     */
+    public async postObjectAsync<TData, TResponseData>(uri: StringOrUrl, data: TData): Promise<TResponseData>;
+    public async postObjectAsync<TData, TResponseData>(uri: StringOrUrl, data: TData): Promise<TResponseData>
+    {
+        const request = this.createRequest(HttpMethod.post, uri);
+
+        request.content = new JsonRequestContent(data);
+        request.headers.update(KnownHeaderNames.accept, "application/json");
+
+        const response = await request.executeAsync();
+
+        if (!response)
+        {
+            throw new RequestCancelledException(request);
+        }
+
+        if (response.cancelled || !response.ok)
+        {
+            throw new ServiceException(response);
+        }
+
+        const content = (await response.contentAsync) as JsonResponseContent;
+        return content.toObject<TResponseData>();
+    }
 }
